c8ae446dc12361ab09c69f2e20c8daeeb42891f6
Carolina Racedo <cracedo@fi.uba.ar>
2026-05-15 20:45:13 -0300

    feat: add Sharder base structure for EOF message handling

--- src/common/worker/sharder.py
+++ src/common/worker/sharder.py
@@ -1,6 +1,16 @@
-from common.worker.worker import Worker
+import threading
+
+from common.worker.stateful_worker import StatefulWorker
 
 
-class Sharder(Worker):
-    def handle_message(self, raw: bytes) -> None:
+class SharderWorker(StatefulWorker):
+    def __init__(self) -> None:
+        super().__init__()
+        self._sent_counts = {}  # (client_id, gateway_id) -> sent_count
+        self._sent_counts_lock = threading.Lock()
+
+    def _handle_control_eof_message(self):
+        pass
+
+    def _handle_eof_message(self):
         pass
